7cc093e058b27c786bb2d77a02e6d88efa79394e
VU Tuan Trung <trung.lespace@gmail.com>
2020-05-18 23:59:22 +0200

    Add multiple5 with tuple

--- PersonalExerice/Exercice2.fs
+++ PersonalExerice/Exercice2.fs
@@ -1,8 +1,17 @@
+// Without tuple
 let fizzBuzz = fun n ->
     match n with
     | x when x % 5 = 0 && x % 3 = 0 -> string "FizzBuzz"
     | x when x % 5 = 0 -> string "Buzz"
     | x when x % 3 = 0 -> string "Fizz"
+    | _ -> string n
+    
+// With tuple
+let fizzBuzzTuple = fun n ->
+    match (n%3, n%5) with
+    | 0,0 -> string "FizzBuzz"
+    | _,0 -> string "Buzz"
+    | 0,_ -> string "Fizz"
     | _ -> string n
     
 //[0..20]
